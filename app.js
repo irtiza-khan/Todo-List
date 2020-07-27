90119902caf4575ec8248cb1d460e14299ca8ac0
irtiza-khan <66550798+irtiza-khan@users.noreply.github.com>
2020-07-26 17:35:43 -0700

    delete and check button is added and css is also changed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Selector 
+//----------Selector 
 
 const todoInput =  document.querySelector(".todo-input");
 const todoBtn =  document.querySelector(".todo-btn");
@@ -6,23 +6,25 @@ const todoList =  document.querySelector(".todo-list");
 
 
 
-//Event Listeners
+//-----------------Event Listeners
 
 todoBtn.addEventListener("click",addTolist)
+todoList.addEventListener("click",deleteCheck);
 
 
-//Functions
+//---------------------Functions
 
-function addTolist(event) {
+     function addTolist(event) {
     //prevent form from submittin
     event.preventDefault();
-    console.log("hello world");
     //TODO  DIV 
     const todoDiv = document.createElement("div"); 
      todoDiv.classList.add("todo"); 
+    //todoDiv.style.background = "red";
+
      // TOD LI
      const newTodo = document.createElement("li");
-     newTodo.innerText = "hello world";
+     newTodo.innerText = todoInput.value;
      newTodo.classList.add("todo-item");
      todoDiv.appendChild(newTodo); //this will add li in div 
 
@@ -30,17 +32,46 @@ function addTolist(event) {
      //ChecK BUTTON
 
      const checkButton =  document.createElement("button");
-     checkButton.innerHTML = '<li class = "fas fa-check"></li>';
+     checkButton.innerHTML = '<i class = "fas fa-check"></i>';
      checkButton.classList.add("complete-btn"); 
      todoDiv.appendChild(checkButton);
 
      //DELET BUTTON
-     const checkButton =  document.createElement("button");
-     checkButton.innerHTML = '<li class = "fas fa-check"></li>';
-     checkButton.classList.add("complete-btn"); 
-     todoDiv.appendChild(checkButton);
-
-
-    
+     const deleteButton =  document.createElement("button");
+     deleteButton.innerHTML = '<i class = "fas fa-trash"></i>';
+     deleteButton.classList.add("delete-btn"); 
+     todoDiv.appendChild(deleteButton);
       
+     //Append it to the div 
+     todoList.appendChild(todoDiv);
+
+     //Clear todo input value
+
+     todoInput.value = "";
+
+}
+
+function deleteCheck(e){
+    console.log(e.target);
+    const item  = e.target; // getting the iterm what we are click on
+      
+    if(item.classList[0] === "delete-btn")
+    {
+        const todo = item.parentElement;
+        todo.classList.add("fall");
+        addEventListener("transitionend", function(){
+           todo.remove();
+        });
+    }
+
+
+    ///Check Mark
+    if(item.classList[0] === "complete-btn")
+    {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+         //alert("Task Has been Completed");
+        //todo.remove();
+    }
+
 }
